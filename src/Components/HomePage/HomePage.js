80a9118bfa736c0d82e32deec5f6d27a3b687f0d
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-21 20:00:55 -0700

    Adds components and basic styling for an area to display a list of articlces

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import '../HomePage/HomePage.css'
 import Header from '../Header/Header'
+import ListDisplay from '../ListDisplay/ListDisplay'
 
 const HomePage = () => {
   return (
-    <div>
+    <main className='home-page'>
       <Header />
-
-    </div>
+      <h2>Articles:</h2>
+      <ListDisplay />
+    </main>
   )
 }
 
---
+++ src/Components/ListDisplay/ListDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../ListDisplay/ListDisplay.css'
+
+const ListDisplay = () => {
+  return (
+    <section className='list-display'>
+      
+    </section>
+  )
+}
+
+export default ListDisplay
