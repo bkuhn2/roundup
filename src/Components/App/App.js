a1fade3c6d8fe134cf2fb1d0bac9d1a64f40c6e1
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-21 20:54:47 -0700

    Clean up functions written and sets state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,21 +4,35 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import ArticleDetailsPage from '../ArticleDetailsPage/ArticleDetailsPage'
 import { getArticles } from '../../Utility/APIcalls';
+import { formatRawData } from '../../Utility/CleanUp';
 
 
 const App = () => {
 
   const [allArticles, setAllArticles] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     Promise.all([
-      getArticles('sports'),
-      getArticles('travel'),
-      getArticles('arts')
+      getArticles('sports'), getArticles('travel'), getArticles('arts')
     ])
       .then(data => {
-        console.log('PromiseAll DATA: ', data);
+        setError('');
+        setAllArticles([...formatRawData(data[0]), ...formatRawData(data[1]), ...formatRawData(data[2])
+        ].sort((a, b) => {
+          if (a.title < b.title) {
+            return -1;
+          }
+          if (a.title > b.title) {
+            return 1;
+          }
+          return 0;
+        }))
       })
+      .catch(err => {
+        setAllArticles([]);
+        setError(err.message);
+      });
   }, []);
 
   return (
