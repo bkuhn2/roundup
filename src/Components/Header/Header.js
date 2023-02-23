0d899a68dc84be94bb5c41671605d42f2454ae6f
Brett Kuhn <brettkuhn87@gmail.com>
2023-02-23 11:27:03 -0700

    Updates Tagline

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <Link to='/'>
         <h1 className='site-title'>Round Up</h1>
       </Link>
-      <p className='tagline'>The News You Actually Want To Read</p>
+      <p className='tagline'>The News You Actually Want To Read: Art, Travel, Sports</p>
     </header>
   )
 }
